73266f20b808d669822077c2e60853d8befb737d
Jieyuan Chen <jc6102@columbia.edu>
2023-10-31 21:23:32 -0400

    HW07 Updated

--- tests/test_prime_jc6102.py
+++ tests/test_prime_jc6102.py
@@ -1,1 +1,7 @@
-from prime_jc6102 import prime_jc6102
+from prime_jc6102 import is_prime
+import pytest
+
+def test_is_prime():
+    prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+    for num in prime_numbers:
+        assert is_prime(num), f"Failed for {num}, expected to be Prime"
